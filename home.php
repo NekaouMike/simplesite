dcb8694666301717221fbb374fb78edb1becced6
EmeraldMike <36397097+EmeraldMike@users.noreply.github.com>
2021-03-15 10:14:32 -0400

    Create home.php

---
+++ home.php
@@ -0,0 +1,9 @@
+//our header will do all the import stuff as all pages inhert what index.php has
+<body>
+//H1 good for a visual title
+<h1>Home</h1>
+//Paragraphs good to tell what this page is about
+<p>Sample Text</p>
+//Buttons humans like clicking on them. Also using the <a> tag we can send them to a different page
+  
+</body>
